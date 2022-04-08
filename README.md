8d2474a93d7595ba2a092577049b118510875206
captain <capitan.develop@gmail.com>
2022-04-08 07:43:51 +0300

    lesson1

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+npm install
+npx hardhat test
+```
---
+++ contracts/Token.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+contract Token {
+    string public constant name = "Cap Token";
+    string public constant symbol = "CAP";
+    uint8  public constant decimals = 1;
+    uint256 public totalSupply = 10000;
+
+    address public owner;
+
+    mapping(address => uint256) internal balances;
+
+    // --- Math ---
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amt) external {
+        require(balances[msg.sender] >= amt, "Not enough tokens");
+
+        balances[msg.sender] = sub(balances[msg.sender], amt);
+        balances[to] = add(balances[to], amt);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
